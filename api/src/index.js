0d8cc8573df6b6a2e539465b4955f0572837259c
ashley <fellguto@gmail.com>
2022-06-24 14:29:01 -0300

    agr vai

--- api/src/index.js
+++ api/src/index.js
@@ -1,6 +1,6 @@
 import 'dotenv/config'
 
-import vilaocontroller from './src/controller/vilaocontroller.js'
+import vilaocontroller from '../src/controller/vilaocontroller.js'
 
 import express from 'express'
 import cors from 'cors'
@@ -9,7 +9,7 @@ import cors from 'cors'
 const server = express();
 server.use(cors());
 server.use(express.json());
-server.use(animecontroller);
+server.use(vilaocontroller);
 
 server.listen(process.env.PORT, () => console.log(`API conectado na porta ${process.env.PORT}`));
 
